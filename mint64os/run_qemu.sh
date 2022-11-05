f72072ee8280a1cc5291c83443f1a61a152b42f1
c0wb3ll <c0wb3ll@naver.com>
2022-11-06 00:31:02 +0900

    llvm chap#5 and mint64 move

---
+++ mint64os/run_qemu.sh
@@ -0,0 +1,18 @@
+O_DEBUG="no"
+O_ARCH="x86_64"
+
+while getopts da: opts; do
+    case $opts in
+    d) O_DEBUG="yes"
+        ;;
+    a) O_ARCH=$OPTARG
+        ;;
+    esac
+done
+
+if [ "${O_DEBUG}" = "yes" ]; then
+    qemu-system-${O_ARCH} -L . -m 64 -fda /mnt/d/home/redcat/mint64os/Disk.img -rtc base=localtime -M pc -s -S
+fi
+if [ "${O_DEBUG}" = "no" ]; then
+    qemu-system-${O_ARCH} -L . -m 64 -fda /mnt/d/home/redcat/mint64os/Disk.img -rtc base=localtime -M pc
+fi
