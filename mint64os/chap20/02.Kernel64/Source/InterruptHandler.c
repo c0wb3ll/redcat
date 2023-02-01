236712e1f767c7a9d8e65e6d5ba72cbef7616f98
c0wb3ll <48816875+c0wb3ll@users.noreply.github.com>
2023-02-01 20:31:01 +0900

    Chap#20 Doing..."

---
+++ mint64os/chap20/02.Kernel64/Source/InterruptHandler.c
@@ -0,0 +1,90 @@
+#include "InterruptHandler.h"
+#include "PIC.h"
+#include "Keyboard.h"
+#include "Console.h"
+#include "Utility.h"
+#include "Task.h"
+#include "Descriptor.h"
+
+void kCommonExceptionHandler( int iVectorNumber, QWORD qwErrorCode ) {
+
+    char vcBuffer[ 3 ] = { 0, };
+
+    vcBuffer[ 0 ] = '0' + iVectorNumber / 10;
+    vcBuffer[ 1 ] = '0' + iVectorNumber % 10;
+
+    kPrintStringXY( 0, 0, "==================================================");
+    kPrintStringXY( 0, 1, "                  Exception Occur~!!!!            ");
+    kPrintStringXY( 0, 2, "                      Vector:                     ");
+    kPrintStringXY( 31, 2, vcBuffer );
+    kPrintStringXY( 0, 3, "==================================================");
+
+    while ( 1 );
+
+}
+
+void kCommonInterruptHandler( int iVectorNumber ) {
+
+    char vcBuffer[] = "[INT:  , ]";
+    static int g_iCommonInterruptCount = 0;
+
+    vcBuffer[ 5 ] = '0' + iVectorNumber / 10;
+    vcBuffer[ 6 ] = '0' + iVectorNumber % 10;
+
+    vcBuffer[ 8 ] = '0' + g_iCommonInterruptCount;
+    g_iCommonInterruptCount = ( g_iCommonInterruptCount + 1 ) % 10;
+    kPrintStringXY( 70, 0, vcBuffer);
+
+    kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
+
+}
+
+void kKeyboardHandler( int iVectorNumber ) {
+
+    char vcBuffer[] = "[INT:  , ]";
+    static int g_iKeyboardInterruptCount = 0;
+    BYTE bTemp;
+
+    vcBuffer[ 5 ] = '0' + iVectorNumber / 10;
+    vcBuffer[ 6 ] = '0' + iVectorNumber % 10;
+
+    vcBuffer[ 8 ] = '0' + g_iKeyboardInterruptCount;
+    g_iKeyboardInterruptCount = ( g_iKeyboardInterruptCount + 1 ) % 10;
+    kPrintStringXY( 0, 0, vcBuffer);
+
+    if( kIsOutputBufferFull() == TRUE ) {
+
+        bTemp = kGetKeyboardScanCode();
+        kConvertScanCodeAndPutQueue( bTemp );
+        
+    }
+
+    kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
+
+}
+
+// 타이머 인터럽트 핸들러
+void kTimerHandler( int iVectorNumber ) {
+
+    char vcBuffer[] = "[INT:  , ]";
+    static int g_iTimerInterruptCount = 0;
+
+    vcBuffer[ 5 ] = '0' + iVectorNumber / 10;
+    vcBuffer[ 6 ] = '0' + iVectorNumber % 10;
+    vcBuffer[ 8 ] = '0' + g_iTimerInterruptCount;
+    g_iTimerInterruptCount = ( g_iTimerInterruptCount + 1 ) % 10;
+    kPrintStringXY( 70, 0, vcBuffer );
+
+    kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
+
+    g_qwTickCount++;
+
+    kDecreaseProcessorTime();
+
+    if( kIsProcessorTimeExpired() == TRUE ) {
+
+        kScheduleInInterrupt();
+
+    }
+
+}
---
+++ mint64os/chap20/02.Kernel64/Source/Synchronization.c
@@ -0,0 +1,75 @@
+#include "Synchronization.h"
+#include "Utility.h"
+#include "Task.h"
+
+// 시스템 전역에서 사용하는 데이터를 위한 잠금 함수
+BOOL kLockForSystemData( void ) {
+
+    return kSetInterruptFlag( FALSE );
+
+}
+
+// 잠금 해제 함수
+void kUnlockForSystemData( BOOL bInterruptFlag ) {
+
+    kSetInterruptFlag( bInterruptFlag );
+
+}
+
+// 뮤텍스 초기화
+void kIntializeMutex( MUTEX* pstMutex ) {
+
+    pstMutex->bLockFlag = FALSE;
+    pstMutex->dwLockCount = 0;
+    pstMutex->qwTaskID = TASK_INVALIDID;
+
+}
+
+// 태스크 사이에서 사용하는 데이터를 위한 잠금 함수
+void kLock( MUTEX* pstMutex ) {
+
+    if( kTestAndSet( &( pstMutex->bLockFlag ), 0, 1 ) == FALSE ) {
+
+        if( pstMutex->qwTaskID == kGetRunningTask()->stLink.qwID ) {
+
+            pstMutex->dwLockCount++;
+            
+            return ;
+
+        }
+
+        while( kTestAndSet( &( pstMutex->bLockFlag ), 0, 1 ) == FALSE ) {
+
+            kSchedule();
+
+        }
+
+    }
+
+    pstMutex->dwLockCount = 1;
+    pstMutex->qwTaskID = kGetRunningTask()->stLink.qwID;
+
+}
+
+// 잠금 해제 함수 2
+void kUnlock( MUTEX* pstMutex ) {
+
+    if( ( pstMutex->bLockFlag == FALSE ) || ( pstMutex->qwTaskID != kGetRunningTask()->stLink.qwID ) ) {
+
+        return ;
+
+    }
+
+    if( pstMutex->dwLockCount > 1 ) {
+
+        pstMutex->dwLockCount--;
+
+        return ;
+
+    }
+
+    pstMutex->qwTaskID = TASK_INVALIDID;
+    pstMutex->dwLockCount = 0;
+    pstMutex->bLockFlag = FALSE;
+
+}
