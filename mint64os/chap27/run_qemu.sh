dcc212e6a9a7c9570e08a9e05e3ab8f63967effc
c0wb3ll <c0wb3ll@naver.com>
2023-06-04 16:24:59 +0900

    Chap27 Start

---
+++ mint64os/chap27/run_qemu.sh
@@ -0,0 +1,19 @@
+O_DEBUG="no"
+O_ARCH="x86_64"
+
+while getopts da: opts; do
+    case $opts in
+    d) O_DEBUG="yes"
+        ;;
+    a) O_ARCH=$OPTARG
+        ;;
+    esac
+done
+
+if [ "${O_DEBUG}" = "yes" ]; then
+    qemu-system-${O_ARCH} -L . -m 64 -fda ${PWD}/Disk.img -hda ${PWD}/HDD.img -rtc base=localtime -M pc -s -S
+fi
+if [ "${O_DEBUG}" = "no" ]; then
+    qemu-img create HDD.img 20M
+    qemu-system-${O_ARCH} -L . -m 64 -fda ${PWD}/Disk.img -hda ${PWD}/HDD.img -rtc base=localtime -M pc
+fi
