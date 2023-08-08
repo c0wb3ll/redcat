ef78071396c78f6d8a684df3e8fe1cdfea09608c
c0wb3ll <c0wb3ll@naver.com>
2023-08-09 00:53:40 +0900

    Chap54 Start

---
+++ mint64os/chap54/02.Kernel64/Source/CacheManager.c
@@ -0,0 +1,256 @@
+#include "CacheManager.h"
+#include "FileSystem.h"
+#include "DynamicMemory.h"
+
+static CACHEMANAGER gs_stCacheManager;
+
+// 파일 시스템 캐시를 초기화
+BOOL kInitializeCacheManager( void ) {
+
+    int i;
+
+    kMemSet( &gs_stCacheManager, 0, sizeof( gs_stCacheManager ) );
+
+    gs_stCacheManager.vdwAccessTime[ CACHE_CLUSTERLINKTABLEAREA ] = 0;
+    gs_stCacheManager.vdwAccessTime[ CACHE_DATAAREA ] = 0;
+
+    gs_stCacheManager.vdwMaxCount[ CACHE_CLUSTERLINKTABLEAREA ] = CACHE_MAXCLUSTERLINKTABLEAREACOUNT;
+    gs_stCacheManager.vdwMaxCount[ CACHE_DATAAREA ] = CACHE_MAXDATAAREACOUNT;
+
+    gs_stCacheManager.vpbBuffer[ CACHE_CLUSTERLINKTABLEAREA ] = ( BYTE* ) kAllocateMemory( CACHE_MAXCLUSTERLINKTABLEAREACOUNT * 512 );
+    if( gs_stCacheManager.vpbBuffer[ CACHE_CLUSTERLINKTABLEAREA ] == NULL ) {
+
+        return FALSE;
+
+    }
+
+    for( i = 0; i < CACHE_MAXCLUSTERLINKTABLEAREACOUNT; i++ ) {
+
+        gs_stCacheManager.vvstCacheBuffer[ CACHE_CLUSTERLINKTABLEAREA ][ i ].pbBuffer = gs_stCacheManager.vpbBuffer[ CACHE_CLUSTERLINKTABLEAREA ] + ( i * 512 );
+
+        gs_stCacheManager.vvstCacheBuffer[ CACHE_CLUSTERLINKTABLEAREA ][ i ].dwTag = CACHE_INVALIDTAG;
+
+    }
+
+    gs_stCacheManager.vpbBuffer[ CACHE_DATAAREA ] = ( BYTE* ) kAllocateMemory( CACHE_MAXDATAAREACOUNT * FILESYSTEM_CLUSTERSIZE );
+    if( gs_stCacheManager.vpbBuffer[ CACHE_DATAAREA ] == NULL ) {
+
+        kFreeMemory( gs_stCacheManager.vpbBuffer[ CACHE_CLUSTERLINKTABLEAREA ] );
+        return FALSE;
+
+    }
+
+    for( i = 0; i < CACHE_MAXDATAAREACOUNT; i++ ) {
+
+        gs_stCacheManager.vvstCacheBuffer[ CACHE_DATAAREA ][ i ].pbBuffer = gs_stCacheManager.vpbBuffer[ CACHE_DATAAREA ] + ( i * FILESYSTEM_CLUSTERSIZE );
+
+        gs_stCacheManager.vvstCacheBuffer[ CACHE_DATAAREA ][ i ].dwTag = CACHE_INVALIDTAG;
+
+    }
+
+    return TRUE;
+
+}
+
+// 캐시 버퍼에서 빈 것을 찾아서 반환
+CACHEBUFFER* kAllocateCacheBuffer( int iCacheTableIndex ) {
+
+    CACHEBUFFER* pstCacheBuffer;
+    int i;
+
+    if( iCacheTableIndex > CACHE_MAXCACHETABLEINDEX ) {
+
+        return FALSE;
+
+    }
+
+    kCutDownAccessTime( iCacheTableIndex );
+
+    pstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ]; i++ ) {
+
+        if( pstCacheBuffer[ i ].dwTag == CACHE_INVALIDTAG ) {
+
+            pstCacheBuffer[ i ].dwTag == CACHE_INVALIDTAG - 1;
+
+            pstCacheBuffer[ i ].dwAccessTime = gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ]++;
+
+            return &( pstCacheBuffer[ i ] );
+
+        }
+
+    }
+
+    return NULL;
+
+}
+
+// 캐시 버퍼에서 태그가 일치하는 것을 찾아서 반환 
+CACHEBUFFER* kFindCacheBuffer( int iCacheTableIndex, DWORD dwTag ) {
+
+    CACHEBUFFER* pstCacheBuffer;
+    int i;
+
+    if( iCacheTableIndex > CACHE_MAXCACHETABLEINDEX ) {
+
+        return FALSE;
+
+    }
+
+    kCutDownAccessTime( iCacheTableIndex );
+
+    pstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ]; i++ ) {
+
+        if( pstCacheBuffer[ i ].dwTag == dwTag ) {
+
+            pstCacheBuffer[ i ].dwAccessTime = gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ]++;
+
+            return &( pstCacheBuffer[ i ] );
+
+        }
+
+    }
+
+    return NULL;
+
+}
+
+// 접근한 시간을 전체적으로 낮춤
+static void kCutDownAccessTime( int iCacheTableIndex ) {
+
+    CACHEBUFFER stTemp;
+    CACHEBUFFER* pstCacheBuffer;
+    BOOL bSorted;
+    int i, j;
+
+    if( iCacheTableIndex > CACHE_MAXCACHETABLEINDEX ) {
+
+        return ;
+        
+    }
+
+    if( gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ] < 0xfffffffe ) {
+
+        return ;
+
+    }
+
+    pstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    for( j = 0; j < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ] - 1; j++ ) {
+
+        bSorted = TRUE;
+        for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ] - 1 - j; i++ ) {
+
+            if( pstCacheBuffer[ i ].dwAccessTime > pstCacheBuffer[ i + 1 ].dwAccessTime ) {
+
+                bSorted = FALSE;
+                
+                kMemCpy( &stTemp, &( pstCacheBuffer[ i ] ), sizeof( CACHEBUFFER ) );
+                kMemCpy( &( pstCacheBuffer[ i ] ), &( pstCacheBuffer[ i + 1 ] ), sizeof( CACHEBUFFER ) );
+                kMemCpy( &( pstCacheBuffer[ i + 1 ] ), &stTemp, sizeof( CACHEBUFFER ) );
+
+            }
+
+        }
+
+        if( bSorted == TRUE ) {
+
+            break;
+
+        }
+
+    }
+
+    for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ]; i++ ) {
+
+        pstCacheBuffer[ i ].dwAccessTime = i;
+
+    }
+
+    gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ] = i;
+
+}
+
+// 캐시 버퍼에서 내보낼 것을 찾음
+CACHEBUFFER* kGetVictimInCacheBuffer( int iCacheTableIndex ) {
+
+    DWORD dwOldTime;
+    CACHEBUFFER* pstCacheBuffer;
+    int iOldIndex;
+    int i;
+
+    if( iCacheTableIndex > CACHE_MAXCACHETABLEINDEX ) {
+
+        return FALSE;
+
+    }
+
+    iOldIndex = -1;
+    dwOldTime = 0xFFFFFFFF;
+
+    pstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ]; i++ ) {
+
+        if( pstCacheBuffer[ i ].dwTag == CACHE_INVALIDTAG ) {
+
+            iOldIndex = i;
+            break;
+
+        }
+
+        if( pstCacheBuffer[ i ].dwAccessTime < dwOldTime ) {
+
+            dwOldTime = pstCacheBuffer[ i ].dwAccessTime;
+            iOldIndex = i;
+
+        }
+
+    }
+
+    if( iOldIndex == -1 ) {
+
+        kPrintf( "Cache Buffer Find Error\n" );
+
+        return NULL;
+
+    }
+
+    pstCacheBuffer[ iOldIndex ].dwAccessTime = gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ]++;
+
+    return &( pstCacheBuffer[ iOldIndex ] );
+
+}
+
+// 캐시버퍼에 내용을 모두 비움
+void kDiscardAllCacheBuffer( int iCacheTableIndex ) {
+
+    CACHEBUFFER* pstCacheBuffer;
+    int i;
+
+    pstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    for( i = 0; i < gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ]; i++ ) {
+
+        pstCacheBuffer[ i ].dwTag = CACHE_INVALIDTAG;
+
+    }
+
+    gs_stCacheManager.vdwAccessTime[ iCacheTableIndex ] = 0;
+
+}
+
+// 캐시 버퍼의 포인터와 최대 개수를 반환
+BOOL kGetCacheBufferAndCount( int iCacheTableIndex, CACHEBUFFER** ppstCacheBuffer, int* piMaxCount ) {
+
+    if( iCacheTableIndex > CACHE_MAXCACHETABLEINDEX ) {
+
+        return FALSE;
+
+    }
+
+    *ppstCacheBuffer = gs_stCacheManager.vvstCacheBuffer[ iCacheTableIndex ];
+    *piMaxCount = gs_stCacheManager.vdwMaxCount[ iCacheTableIndex ];
+    
+    return TRUE;
+
+}
---
+++ mint64os/chap54/03.Application/03.Hexa/Source/Main.h
@@ -0,0 +1,57 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define BOARDWIDTH              8
+#define BOARDHEIGHT             12
+
+#define BLOCKSIZE               32
+#define BLOCKCOUNT              3
+#define EMPTYBLOCK              0
+#define ERASEBLOCK              0xFF
+#define BLOCKKIND               5
+
+#define WINDOW_WIDTH            ( BOARDWIDTH * BLOCKSIZE )
+#define WINDOW_HEIGHT           ( WINDOW_TITLEBAR_HEIGHT + INFORMATION_HEIGHT + BOARDHEIGHT * BLOCKSIZE )
+
+#define INFORMATION_HEIGHT      20
+
+// 구조체
+typedef struct GameInfoStruct {
+
+    COLOR vstBlockColor[ BLOCKKIND + 1 ];
+    COLOR vstEdgeColor[ BLOCKKIND + 1 ];
+
+    int iBlockX;
+    int iBlockY;
+
+    BYTE vvbBoard[ BOARDHEIGHT ][ BOARDWIDTH ];
+
+    BYTE vvbEraseBlock[ BOARDHEIGHT ][ BOARDWIDTH ];
+
+    BYTE vbBlock[ BLOCKCOUNT ];
+
+    BOOL bGameStart;
+
+    QWORD qwScore;
+
+    QWORD qwLevel;
+
+} GAMEINFO;
+
+// 함수
+void Initialize( void );
+void CreateBlock( void );
+BOOL IsMovePossible( int iBlockX, int iBlockY );
+BOOL FreezeBlock( int iBlockX, int iBlockY );
+void EraseAllContinuousBlockOnBoard( QWORD qwWindowID );
+
+BOOL MarkContinuousVerticalBlockOnBoard( void );
+BOOL MarkContinuousHorizonBlockOnBoard( void );
+BOOL MarkContinuousDiagonalBlockInBoard( void );
+void EraseMarkedBlock( void );
+void CompactBlockOnBoard( void );
+
+void DrawInformation( QWORD qwWindowID );
+void DrawGameArea( QWORD qwWindowID );
+
+#endif /*__MAIN_H__*/
